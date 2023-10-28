7ade8c0c5603ad20c94fd77c01ecf87a13776b15
George Fotopoulos <xorz57@gmail.com>
2023-10-28 15:53:19 +0300

    Update Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -11,6 +11,7 @@ int main() {
     std::cout << "Search 'apple': " << (trie.Search("apple") ? "Found" : "Not Found") << std::endl;
     std::cout << "Search 'app': " << (trie.Search("app") ? "Found" : "Not Found") << std::endl;
     std::cout << "Search 'ap': " << (trie.Search("ap") ? "Found" : "Not Found") << std::endl;
+
     std::cout << "StartsWith 'app': " << (trie.StartsWith("app") ? "Found" : "Not Found") << std::endl;
     std::cout << "StartsWith 'bat': " << (trie.StartsWith("bat") ? "Found" : "Not Found") << std::endl;
 
